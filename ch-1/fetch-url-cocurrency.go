d89362b8827d60a6cfcb29b8abd669b5f7af59e1
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-01-10 16:59:47 -0300

    cocurrency with go

---
+++ ch-1/fetch-url-cocurrency.go
@@ -0,0 +1,40 @@
+import (
+	"ftm"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start go routine
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // recive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed \n",  time.Since(start).Seconds())
+}
+
+func fetch(url, string, ch chan<- string ) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Srint(err) // send to channel ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.body )
+	resp.body.Close() // do not leak resources
+	if(err != nil ) {
+		ch <- fmt.Sprintf("while reading %s: %v: " url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%2fs %7d %s", secs, nbytes, url)
+}
