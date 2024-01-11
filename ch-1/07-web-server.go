73e585de0e46a908a692fed51cdf45f0acc82c01
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-01-10 22:00:27 -0300

    web server in go

---
+++ ch-1/07-web-server.go
@@ -0,0 +1,31 @@
+package (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// Handler echoes the Path component of the request URL
+func handler(w http.ResponseWriter, r, *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "urlPath: = %q\n" r.URL.Path)
+}
+
+// Counter echoes the number of calls so far
+func counter(w http.ResponseWritter, r, *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
